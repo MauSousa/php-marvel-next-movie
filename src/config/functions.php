9455e3394cd242fa6ac22a54ebc37629a296fe65
Augusto Sousa <lagrange.tesla@gmail.com>
2024-04-14 22:23:55 -0600

    Add functions to change date to MX format and to get data from api

--- src/config/functions.php
+++ src/config/functions.php
@@ -11,7 +11,31 @@ function load_env()
 
 function dede(string|array $text)
 {
-    echo '<pre>';
-    var_export($text);
+    echo '<pre class="bg-secondary-subtle text-primary-emphasis fw-bold shadow-lg rounded fade-in p-5 mb-3">';
+        var_export($text);
     echo '</pre>';
+}
+
+function get_apit_data()
+{
+    # Se inicia la sesión de cURL
+    $ch = curl_init($_ENV['API_URL']);
+
+    # Indicar que queremos recibir el resultado pero no la mostramos en pantalla
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    # Ejecutamos la petición
+    $result = curl_exec($ch);
+    $data = json_decode($result, true);
+
+    # Cerramos cURL
+    curl_close($ch);
+
+    return $data;
+}
+
+function convert_date(string $date)
+{
+    $date = explode('-', $date);
+    return $date[2] . '-' . $date[1] . '-' . $date[0];
 }
